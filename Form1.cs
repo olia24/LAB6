adb6b451c3be5577fb20585f2d4c44bc2dc5bc26
olia24 <79983053+olia24@users.noreply.github.com>
2021-06-15 01:07:14 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TV_Library;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        List<TV_Library.TV> DataList = new List<TV>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TV_ObjectBuffer.TV_BufferObject = new TV_Library.TV(new TV_Library.TV.s_TV_Desc());
+            Form2 newWindow = new Form2();
+            newWindow.ShowDialog();
+            if (TV_ObjectBuffer.TV_BufferObject != null)
+                this.DataList.Add(TV_ObjectBuffer.TV_BufferObject);
+            UpdateDGVData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataList.Clear();
+            UpdateDGVData();
+        }
+        private void UpdateDGVData()
+        {
+            DGV_Data.Rows.Clear();
+            int rowCount = DataList.Count;
+            for (int row = 0; row < rowCount; row++)
+            {
+                DGV_Data.Rows.Add();
+                DGV_Data.Rows[row].Cells[0].Value = DataList[row].DataForDGV()[0];
+                DGV_Data.Rows[row].Cells[1].Value = DataList[row].DataForDGV()[1];
+                DGV_Data.Rows[row].Cells[2].Value = DataList[row].DataForDGV()[2];
+                DGV_Data.Rows[row].Cells[3].Value = DataList[row].DataForDGV()[3];
+                DGV_Data.Rows[row].Cells[4].Value = DataList[row].DataForDGV()[4];
+                DGV_Data.Rows[row].Cells[5].Value = DataList[row].DataForDGV()[5];
+                DGV_Data.Rows[row].Cells[6].Value = DataList[row].DataForDGV()[6];
+                DGV_Data.Rows[row].Cells[7].Value = DataList[row].DataForDGV()[7];
+            }
+        }
+    }
+    public class TV_ObjectBuffer
+    {
+        public static TV_Library.TV TV_BufferObject { get; set; }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form2 : Form
+    {
+            TV_Library.TV BufferObject;
+            TV_Library.TV.s_TV_Desc BDesc;
+            public Form2()
+            {
+                InitializeComponent();
+
+                BufferObject = new TV_Library.TV(new TV_Library.TV.s_TV_Desc());
+
+                label1.Text = BufferObject.ParamsName()[0];
+                label2.Text = BufferObject.ParamsName()[1];
+                label3.Text = BufferObject.ParamsName()[2];
+                label4.Text = BufferObject.ParamsName()[3];
+                label5.Text = BufferObject.ParamsName()[4];
+                label6.Text = BufferObject.ParamsName()[5];
+                label7.Text = BufferObject.ParamsName()[6];
+                label8.Text = BufferObject.ParamsName()[7];
+            }
+
+            private void B_Cancel_Click(object sender, System.EventArgs e)
+            {
+                BufferObject = null;
+                this.Close();
+            }
+
+            private void B_Save_Click(object sender, System.EventArgs e)
+            {
+                BDesc.width = Convert.ToInt32(textBox1.Text);
+                BDesc.height = Convert.ToInt32(textBox2.Text);
+                BDesc.diagonal = Convert.ToInt32(textBox4.Text);
+                BDesc.resolution.X_Res = Convert.ToInt32(textBox3.Text); BDesc.resolution.Y_Res = Convert.ToInt32(textBox9.Text);
+                BDesc.modelName = (textBox5.Text);
+                BDesc.manufacturer = (textBox6.Text);
+                BDesc.price = Convert.ToInt32(textBox6.Text.Replace('.', ','));
+                BDesc.weight = Convert.ToInt32(textBox7.Text);
+
+                BufferObject = new TV_Library.TV(BDesc);
+                TV_ObjectBuffer.TV_BufferObject = BufferObject;
+                this.Close();
+            }
+        }
+}
